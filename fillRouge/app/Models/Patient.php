2068cc1986ea28b31d8ced4293b6aa2b7c1d8e9f
a-oirgari <abdrahmanghali1998@gmail.com>
2026-03-13 11:10:41 +0000

    chore: the patient model

---
+++ fillRouge/app/Models/Patient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'phone', 'address'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function completedAppointments()
+    {
+        return $this->appointments()->where('status', 'completed')->with('consultation');
+    }
+}
