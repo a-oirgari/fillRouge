bd221e9e6619ad1408bc7920bf23a4b725319325
a-oirgari <abdrahmanghali1998@gmail.com>
2026-03-13 11:04:15 +0000

    feat: the files of the migration

---
+++ fillRouge/database/migrations/2026_03_13_105600_create_doctors_table.php.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('city')->nullable();
+            $table->text('bio')->nullable();
+            $table->boolean('validated')->default(false);
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
