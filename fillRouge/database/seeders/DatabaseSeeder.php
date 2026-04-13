89d22267900758847c52b5a15d18edde4dcc0394
a-oirgari <abdrahmanghali1998@gmail.com>
2026-04-13 09:46:55 +0100

    feat: implement DatabaseSeeder to populate users, specialities, doctors, and availabilities

--- fillRouge/database/seeders/DatabaseSeeder.php
+++ fillRouge/database/seeders/DatabaseSeeder.php
@@ -71,9 +71,75 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
+        $doctorsData = [
+            [
+                'name' => 'Dr. Amine El Fassi',
+                'email' => 'amine@mediconnect.ma',
+                'city' => 'Rabat',
+                'bio' => 'Dermatologue passionné par les maladies de la peau et l\'esthétique.',
+                'phone' => '0633445566',
+                'speciality_id' => 3, 
+                'days' => ['Mardi', 'Jeudi', 'Samedi']
+            ],
+            [
+                'name' => 'Dr. Fatima Zohra',
+                'email' => 'fatima@mediconnect.ma',
+                'city' => 'Marrakech',
+                'bio' => 'Pédiatre attentionnée, accompagnant le développement des enfants depuis plus de 15 ans.',
+                'phone' => '0644556677',
+                'speciality_id' => 4, 
+                'days' => ['Lundi', 'Mardi', 'Jeudi', 'Vendredi']
+            ],
+            [
+                'name' => 'Dr. Youssef Berrada',
+                'email' => 'youssef@mediconnect.ma',
+                'city' => 'Tanger',
+                'bio' => 'Ophtalmologue chirurgien, spécialiste des cataractes et la chirurgie réfractive.',
+                'phone' => '0655667788',
+                'speciality_id' => 7, 
+                'days' => ['Lundi', 'Mercredi']
+            ],
+            [
+                'name' => 'Dr. Leila Mansouri',
+                'email' => 'leila@mediconnect.ma',
+                'city' => 'Casablanca',
+                'bio' => 'Médecin généraliste assurant des consultations globales et le suivi des patients.',
+                'phone' => '0666778899',
+                'speciality_id' => 1, 
+                'days' => ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi']
+            ],
+        ];
+
+        foreach ($doctorsData as $data) {
+            $docUser = User::create([
+                'name'     => $data['name'],
+                'email'    => $data['email'],
+                'password' => Hash::make('password'),
+                'role'     => 'doctor',
+            ]);
+            $doc = Doctor::create([
+                'user_id'   => $docUser->id,
+                'city'      => $data['city'],
+                'bio'       => $data['bio'],
+                'validated' => true,
+                'phone'     => $data['phone'],
+            ]);
+            $doc->specialities()->attach($data['speciality_id']);
+
+            foreach ($data['days'] as $day) {
+                Availability::create([
+                    'doctor_id'  => $doc->id,
+                    'day'        => $day,
+                    'start_time' => '09:00',
+                    'end_time'   => '17:00',
+                ]);
+            }
+        }
+
         $this->command->info(' Seeder terminé!');
         $this->command->info('Admin: admin@mediconnect.ma / password');
         $this->command->info('Patient: patient@mediconnect.ma / password');
         $this->command->info('Médecin: doctor@mediconnect.ma / password');
+        $this->command->info('Autres médecins: amine@..., fatima@..., youssef@..., leila@... / password');
     }
 }
